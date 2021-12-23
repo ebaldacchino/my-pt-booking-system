1de967e1cfab35ce0f1c12ec2b3e91ccd34d60f3
Edward Baldacchino <edward.baldacchino@live.vu.edu.au>
2021-12-23 18:23:06 +1100

    Added notes for potential security headers

--- next.config.js
+++ next.config.js
@@ -1,9 +1,48 @@
-// Similiar to what I'm building for work
-// https://avif.io/blog/tutorials/nextjs/#next.jsimagecomponent
+// https://nextjs.org/docs/advanced-features/security-headers
+
+// const securityHeaders = [
+// 	{
+// 		key: 'X-DNS-Prefetch-Control',
+// 		value: 'on',
+// 	},
+// 	{
+// 		key: 'Strict-Transport-Security',
+// 		value: 'max-age=63072000; includeSubDomains; preload',
+// 	},
+// 	{
+// 		key: 'X-XSS-Protection',
+// 		value: '1; mode=block',
+// 	},
+// 	{
+// 		key: 'X-Frame-Options',
+// 		value: 'SAMEORIGIN',
+// 	},
+// 	{
+// 		key: 'X-Content-Type-Options',
+// 		value: 'nosniff',
+// 	},
+// 	{
+// 		key: 'Referrer-Policy',
+// 		value: 'origin-when-cross-origin',
+// 	},
+// 	{
+// 		key: 'Content-Security-Policy',
+// 		value: "default-src 'self'",
+// 	},
+// ];
 
 module.exports = {
 	reactStrictMode: true,
 	images: {
 		formats: ['image/avif', 'image/webp'],
 	},
+	// async Headers() {
+	// 	return [
+	// 		{
+	// 			// Apply these headers to all routes in your application.
+	// 			source: '/(.*)',
+	// 			headers: securityHeaders,
+	// 		},
+	// 	];
+	// },
 };
