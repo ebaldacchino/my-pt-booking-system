ce5c73be18d5ba0fd736913e64f1954ceb10674a
Edward Baldacchino <edward.baldacchino@live.vu.edu.au>
2022-01-02 06:11:19 +1100

    Refactored code

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -25,6 +25,7 @@ import DateSection from '../../components/book/DateSection';
 import { Button, Variant } from '../../styles/button';
 import schedule from '../../components/book/mockDates';
 import type { GetServerSideProps } from 'next';
+import AvailableSession from '../../components/book/AvailableSession';
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
 	const { props, redirect } = await authUserServerSideProps(context);
@@ -34,25 +35,6 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
 const TimeSection = tw.section`bg-blue-600 text-white flex-1 w-full`;
 const TimeContainer = tw.div`grid sm:grid-cols-2 lg:grid-cols-3 gap-4 w-full mb-auto px-4 p-4`;
-const SlotContainer = tw.div`bg-blue-500 border-[3px] rounded-lg border-blue-200 flex flex-wrap gap-4 w-full justify-between items-center p-4`;
-const Time = tw.span`m-auto`;
-const BookButton = tw(Button)`m-auto`;
-
-const Slot = ({ time }: { time: number }) => {
-	const hr = Math.floor((time - (time >= 1300 ? 1200 : 0)) / 100);
-	const endHr = hr === 12 ? 1 : hr + 1;
-	const min = time % 100;
-	const formattedMin = min < 10 ? `0${min}` : min;
-	return (
-		<SlotContainer>
-			<Time>
-				{hr}:{formattedMin} - {endHr}:{formattedMin}{' '}
-				{time < 1100 || time >= 2300 ? 'AM' : 'PM'}
-			</Time>
-			<BookButton variant={Variant.secondary}>Book Today</BookButton>
-		</SlotContainer>
-	);
-};
 
 interface Props {
 	givenName: string;
@@ -60,23 +42,24 @@ interface Props {
 }
 
 export default function Book(props: Props) {
-	const calendarData = useCalendar({ schedule: props.schedule });
+	const calendar = useCalendar({ schedule: props.schedule });
 	return (
 		<Layout
 			user={props.givenName}
 			title='Book here'
 			description='Number One Personal Training services'>
-			<DateSection {...calendarData} />
+			<DateSection {...calendar} />
 			<TimeSection>
 				<TimeContainer>
-					{calendarData.slots.map(
-						({ time }, index: number) => {
-							return <Slot key={index} time={Number(time)} />;
-						}
-					)}
+					{calendar.slots.map((slot, index: number) => {
+						const { time, sessionLength } = slot;
+						return (
+							<AvailableSession key={index} {...{ time, sessionLength }} />
+						);
+					})}
 				</TimeContainer>
 			</TimeSection>
-			<Calendar {...calendarData} />
+			<Calendar {...calendar} />
 		</Layout>
 	);
 }
