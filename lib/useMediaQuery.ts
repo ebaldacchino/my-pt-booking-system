309a5d2dbc3f0e4e1a50deb764cf9caa843141bd
Edward Baldacchino <edward.baldacchino@live.vu.edu.au>
2022-01-01 22:11:57 +1100

    Fixed useMediaQuery for SSR

--- lib/useMediaQuery.ts
+++ lib/useMediaQuery.ts
@@ -1,18 +1,19 @@
-import React from 'react'
-export const useMediaQuery = () => {
+import React from 'react';
+import useIsomorphicLayoutEffect from './useIsomorphicLayoutEffect';
+const useMediaQuery = () => {
 	const [values, setValues] = React.useState({
 		isDesktop: false,
-		isMobile: true,
+		isMobile: false,
 	});
-	React.useLayoutEffect(() => {
-		if (!window) return;
-		const { innerWidth: w } = window;
+	useIsomorphicLayoutEffect(() => {
 		const handleResize = () => {
+			const { innerWidth: w } = window;
 			setValues({
 				isDesktop: w >= 1024,
 				isMobile: w < 640,
 			});
 		};
+		handleResize();
 		window.addEventListener('resize', handleResize);
 		return () => {
 			window.removeEventListener('resize', handleResize);
@@ -20,3 +21,5 @@ export const useMediaQuery = () => {
 	}, []);
 	return values;
 };
+
+export default useMediaQuery;
